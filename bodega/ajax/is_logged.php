21db4dc8946fea159d3a1d06cbb7a8ceee1dc704
Santiago Usme (via Travis CI) <santiagousm3@gmail.com>
2020-12-07 21:40:13 +0000

    Deploy santiagousm3/bodega to github.com/santiagousm3/bodega.git:gh-pages

--- bodega/ajax/is_logged.php
+++ bodega/ajax/is_logged.php
@@ -1,6 +1,6 @@
 <?php	
 	session_start();
-	if (!isset($_SESSION['user_login_status']) and $_SESSION['user_login_status'] != 1 ) {
+	if (!isset($_SESSION["user_login_status"]) and $_SESSION["user_login_status"] != 1 ) {
         header("location: ../login.php");
 		exit;
     }
