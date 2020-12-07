4ca272cf0b44cefb51f9b1d57fa761208e620cdc
Santiago Usme <santiagousm3@gmail.com>
2020-12-07 16:10:45 -0500

    modificacion en  varaible session

--- bodega/ajax/is_logged.php
+++ bodega/ajax/is_logged.php
@@ -1,6 +1,6 @@
 <?php	
 	session_start();
-	if (!isset($_SESSION['user_login_status']) and $_SESSION['user_login_status'] != 1 and !empty($_SESSION['user_login_status'])) {
+	if (!isset($_SESSION['user_login_status']) and $_SESSION['user_login_status'] != 1 ) {
         header("location: ../login.php");
 		exit;
     }
